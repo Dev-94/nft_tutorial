7ed2fc0420ecbffbadc28a792a7a907344e1adbf
Dev Govindji <dev.govindji.software@gmail.com>
2022-02-21 11:12:43 -0600

    first commit

---
+++ ethereum/contracts/EmotionalShapes.sol
@@ -0,0 +1,28 @@
+// SPDX-Licenese-Identifier: MIT
+pragma solidity 0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract EmotionalShapes is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("EmotionalShapes", "ESS"){}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "http://5aa1-2600-1700-19e2-d30-b590-9a73-7b48-d5bc.ngrok.io/api/erc721/";
+    }
+
+    function mint(address to) 
+        public returns (uint256)
+    {
+        require(_tokenIdCounter.current() < 3);
+        _tokenIdCounter.increment();
+        _safeMint(to, _tokenIdCounter.current());
+
+        return _tokenIdCounter.current();
+    }
+
+
+}
